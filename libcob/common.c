c1c0484765f0fed5acddc41b907eb4c74dcbbd9a
sf-mensch <sf-mensch@localhost>
2017-03-02 21:13:44 +0000

    Merged revisions 1050, 1267, 1279 from branches/gnu-cobol-2.0:
    Follow-up to r756 "Adding FDL text to the documentation"
    * Makefile.am: added all sources to gnucobol_TEXINFOS and removed them from EXTRA_DIST
    ........
    Changing all open references for CBL_OC_xxx system routines to CBL_GC_xxx
    ........
    gnucobol.texi: fixed texinfo warnings
    ........

--- libcob/common.c
+++ libcob/common.c
@@ -4408,12 +4408,12 @@ cob_sys_getopt_long_long (void* so, void* lo, void* idx, const int long_only, vo
 		rtns = (int *)cob_malloc(sizeof(int) * (lo_amount + 1U));
 	}
 	else {
-		cob_runtime_error (_("Call to CBL_OC_GETOPT with wrong longoption size."));
+		cob_runtime_error (_("Call to CBL_GC_GETOPT with wrong longoption size."));
 		cob_stop_run (1);
 	}
 
 	if (!COB_MODULE_PTR->cob_procedure_params[2]) {
-		cob_runtime_error (_("Call to CBL_OC_GETOPT with missing longind."));
+		cob_runtime_error (_("Call to CBL_GC_GETOPT with missing longind."));
 		cob_stop_run (1);
 	}
 	longind = cob_get_int (COB_MODULE_PTR->cob_procedure_params[2]);
