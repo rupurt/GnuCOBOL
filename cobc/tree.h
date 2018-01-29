b4155318fcc43025b9d6bb51bb25f8b833473061
rjnorman74 <rjnorman74@localhost>
2018-01-29 20:32:18 +0000

    Fix for correct arithmetic_osvs

--- cobc/tree.h
+++ cobc/tree.h
@@ -1666,6 +1666,7 @@ extern struct cb_program	*cb_build_program (struct cb_program *,
 
 extern cb_tree		cb_check_numeric_value (cb_tree);
 extern size_t		cb_check_index_p (cb_tree x);
+extern void		cb_set_dmax (int scale);
 
 extern void		cb_build_registers (void);
 extern void		cb_build_debug_item (void);
