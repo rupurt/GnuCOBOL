f382dde5cff1b9e09edc227ff9620778cc65f091
rjnorman74 <rjnorman74@localhost>
2018-03-25 13:38:20 +0000

    Additional fix for UTC compute

--- cobc/tree.c
+++ cobc/tree.c
@@ -708,38 +708,6 @@ get_last_elt (cb_tree l)
 	return l;
 }
 
-#if !defined (COB_STRFTIME) && !defined (COB_TIMEZONE)
-static void
-warn_cannot_get_utc (const cb_tree tree, const enum cb_intr_enum intr,
-			cb_tree args)
-{
-	const char	*data = try_get_constant_data (CB_VALUE (args));
-	int		is_variable_format = data == NULL;
-	int		is_constant_utc_format
-		= data != NULL && strchr (data, 'Z') != NULL;
-	int		is_formatted_current_date
-		= intr == CB_INTR_FORMATTED_CURRENT_DATE;
-	cb_tree		last_arg = get_last_elt (args);
-	int	        has_system_offset_arg
-		= (intr == CB_INTR_FORMATTED_DATETIME
-		   || intr == CB_INTR_FORMATTED_TIME)
-		  && last_arg->tag == CB_TAG_INTEGER
-		  && ((struct cb_integer *) last_arg)->val == 1;
-        #define ERR_MSG _("Cannot find the UTC offset on this system")
-
-	if (!is_formatted_current_date && !has_system_offset_arg) {
-		return;
-	}
-	
-	if (is_variable_format) {
-		cb_warning_x (tree, ERR_MSG);
-	} else if (is_constant_utc_format) {
-		cb_error_x (tree, ERR_MSG);
-	}
-
-        #undef ERR_MSG
-}
-#endif
 
 #if 0 /* Unused routines */
 static int
@@ -4448,9 +4416,6 @@ cb_build_intrinsic (cb_tree name, cb_tree args, cb_tree refmod,
 		if (!valid_const_date_time_args (name, cbp, args)) {
 			return cb_error_node;
 		}
-#if !defined (COB_STRFTIME) && !defined (COB_TIMEZONE)
-		warn_cannot_get_utc (name, cbp->intr_enum, args);
-#endif
 	}
 
 	/*
