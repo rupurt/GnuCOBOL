a4ebd317e6622d649138fe825f2ff334aeeb3826
sf-mensch <sf-mensch@localhost>
2018-08-17 14:45:50 +0000

    * coblocal.h, numeric.c (cob_decimal_init2): fixed compiler warning introduced by [r2664]

--- libcob/coblocal.h
+++ libcob/coblocal.h
@@ -353,7 +353,7 @@ COB_HIDDEN void		cob_exit_strings	(void);
 COB_HIDDEN int		cob_real_get_sign	(cob_field *);
 COB_HIDDEN void		cob_real_put_sign	(cob_field *, const int);
 
-COB_HIDDEN void		cob_decimal_init2	(cob_decimal *, const mp_bitcnt_t);
+COB_HIDDEN void		cob_decimal_init2	(cob_decimal *, const cob_uli_t);
 COB_HIDDEN void		cob_decimal_setget_fld	(cob_field *, cob_field *,
 						 const int);
 COB_HIDDEN void		cob_decimal_move_temp	(cob_field *, cob_field *);
--- libcob/numeric.c
+++ libcob/numeric.c
@@ -292,7 +292,7 @@ cob_binary_set_int64 (cob_field *f, cob_s64_t n)
 /* Decimal number */
 
 void
-cob_decimal_init2 (cob_decimal *d, mp_bitcnt_t initial_num_bits)
+cob_decimal_init2 (cob_decimal *d, const cob_uli_t initial_num_bits)
 {
 	mpz_init2 (d->value, initial_num_bits);
 	d->scale = 0;
