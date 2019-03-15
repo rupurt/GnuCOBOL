0adab9b73344b6426b79bfef549e284302015f00
sf-mensch <sf-mensch@localhost>
2019-03-15 21:07:19 +0000

    * autogen.sh: fixed to work correctly when called as "./autogen.sh"

--- autogen.sh
+++ autogen.sh
@@ -26,15 +26,17 @@ me=autogen.sh
 # get path to GnuCOBOL main directory
 if test "$0" = "./$me"; then
   MAINPATH=.
+  GCMAINPATH=".."
 else
   MAINPATH=$(dirname $(readlink -f "$0"))
+  GCMAINPATH="$MAINPATH"
 fi
 if test ! -f $MAINPATH/$me; then
-  echo; echo ERROR - cannot set main directory - aborting $me && exit 1
+  echo; echo "ERROR - cannot set main directory [checked $MAINPATH/build_aux/$me] - aborting $me" && exit 1
 fi
 
 olddir_autogen=`pwd`
-cd $MAINPATH/build_aux && chmod u+x ./bootstrap && GCMAINPATH=$MAINPATH ./bootstrap; ret=$?
+cd $MAINPATH/build_aux && chmod u+x ./bootstrap && ./bootstrap; ret=$?
 cd $olddir_autogen
 
 if test $ret -ne 0; then
