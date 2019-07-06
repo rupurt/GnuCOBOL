ebadf9fe92fd128a3add431048ac5183a2605c58
sf-mensch <sf-mensch@localhost>
2019-07-06 22:09:08 +0000

    pangaea cleanup
    libcob:
    * common.h: removed inclusion of stdio.h
    * common.h, coblocal.h: moved definition of cob_print_field to local
    * fileio.c (cob_write_dict): fixed compiler warnings

--- libcob/coblocal.h
+++ libcob/coblocal.h
@@ -382,6 +382,8 @@ COB_HIDDEN void		cob_init_move		(cob_global *, cob_settings *);
 COB_HIDDEN void		cob_init_screenio	(cob_global *, cob_settings *);
 COB_HIDDEN void		cob_init_mlio		(cob_global * const);
 
+COB_HIDDEN void		cob_print_field		(FILE *, cob_field *, int, int);
+
 COB_HIDDEN void		cob_fork_fileio		(cob_global *, cob_settings *);
 
 COB_HIDDEN void		cob_exit_screen		(void);
