acd76016d0c8dec8bd30a39aab2edeb31b891d91
sf-mensch <sf-mensch@localhost>
2020-09-30 10:52:19 +0000

    JSON handler update
    follow-up to r3721 "JSON-C as a JSON handler" (see [patches:#45]) and r3700

--- cobc/typeck.c
+++ cobc/typeck.c
@@ -103,7 +103,7 @@ struct cb_statement		*error_statement = NULL;
 #ifndef WITH_XML2
 static int			warn_xml_done = 0;
 #endif
-#ifndef WITH_JSON
+#if	!defined (WITH_CJSON) && !defined (WITH_JSON_C)
 static int			warn_json_done = 0;
 #endif
 #ifndef WITH_EXTENDED_SCREENIO
@@ -13258,7 +13258,7 @@ cb_emit_json_generate (cb_tree out, cb_tree from, cb_tree count,
 	if (current_statement->ex_handler == NULL
 	 && current_statement->not_ex_handler == NULL)
 	  	current_statement->handler_type = NO_HANDLER;
-#ifndef WITH_JSON
+#if	!defined (WITH_CJSON) && !defined (WITH_JSON_C)
 	if (!warn_json_done) {
 		warn_json_done = 1;
 		cb_warning (cb_warn_unsupported,
@@ -13271,9 +13271,9 @@ cb_emit_json_generate (cb_tree out, cb_tree from, cb_tree count,
 		return;
 	}
 
-        tree = CB_ML_TREE (cb_build_ml_tree (CB_FIELD (cb_ref (from)),
-					     0, 0, name_list,
-					     NULL, suppress_list));
+	tree = CB_ML_TREE (cb_build_ml_tree (CB_FIELD (cb_ref (from)),
+						0, 0, name_list,
+						NULL, suppress_list));
 
 	tree->sibling = current_program->ml_trees;
 	current_program->ml_trees = tree;
